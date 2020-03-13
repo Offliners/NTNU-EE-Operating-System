d1951127a5a72bcb123c20859eb362361e50d8d9
Offliner <42777325+Offliners@users.noreply.github.com>
2020-03-13 10:40:14 +0800

    Create int2bin.c

---
+++ Week2/HW1/int2bin.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define MAXSIZE 100000
+
+int main(int argc, char* argv[])
+{
+        char carry[MAXSIZE];
+        int bin[MAXSIZE/10];
+        int i, j = 0, k, n, len, sum = 1;
+
+        if(argc != 2)
+        {
+                printf("Please use command %s [number]", argv[0]);
+                exit(1);
+        }
+
+        len = strlen(argv[1]);
+        carry[0] = '0';
+        for(i = 0; i < len; i++)
+                bin[i] = argv[1][i] - '0';
+        while(sum)
+        {
+                sum = 0;
+                for(i = 0; i < len; i++)
+                {
+                        k = bin[i] / 2;
+                        sum += k;
+                        if(i == len - 1)
+                                carry[j++] = bin[i] % 2 + '0';
+                        else
+                                bin[i+1] += bin[i] % 2 * 10;
+                        bin[i] = k;
+                }
+        }
+        for(j = j - 1; j >= 0; j--)
+                printf("%c",carry[j]);
+        printf("\n");
+        return 0;
+}
