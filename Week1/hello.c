67201f73a2d123c7104f7d66052acf06f0d16dd6
Offliner <42777325+Offliners@users.noreply.github.com>
2020-03-19 19:45:55 +0800

    Create hello.c

---
+++ Week1/hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+        int a, b;
+        int c;
+
+        printf("Please input a, b : ");
+        scanf("%d %d", &a, &b);
+        c = a + b;
+        printf("a + b = %d\n", c);
+
+        return 0;
+}
