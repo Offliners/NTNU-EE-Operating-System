2f6b220ed80ddc06d8b3a415ff23cc1b6c1ef021
Offliner <42777325+Offliners@users.noreply.github.com>
2020-03-23 13:30:33 +0800

    Create quiz2.c

---
+++ Week4/quiz2.c
@@ -0,0 +1,44 @@
+#include <sys/types.h>
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+#include <wait.h>
+
+int main(){
+
+        pid_t pid, pid1;
+
+        pid = fork();
+
+        if(pid<0){
+
+                fprintf(stderr, "Fork failed");
+
+                return 1;
+
+        }
+
+        else if(pid == 0){
+
+                pid1 = getpid();
+
+                printf("child: pid = %d\n", pid);//Line A
+
+                printf("child: pid1 = %d\n", pid1);//Line B
+
+        }
+
+        else{
+
+                pid1 = getpid();
+
+                printf("parent: pid = %d\n", pid);//Line C
+
+                printf("parent: pid1 = %d\n", pid1);//Line D
+
+        wait(NULL);
+
+        }
+}
