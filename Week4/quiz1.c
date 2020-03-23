1f1a2370dd680c6e1cf6ae593ae56d079ed6e496
Offliner <42777325+Offliners@users.noreply.github.com>
2020-03-23 13:30:21 +0800

    Create quiz1.c

---
+++ Week4/quiz1.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+#include <wait.h>
+
+int value = 5;
+
+int main(){
+
+        pid_t pid;
+
+        pid = fork();
+
+        if(pid == 0){
+
+                value += 15;
+
+        }
+
+        else{
+
+                wait(NULL);
+
+                printf("Parent: value: %d\n", value);
+
+        }
+
+        return 0;
+
+}
