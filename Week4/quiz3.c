2dbdea6a2115dd9f09eb0c0a6bfc8e5c852c7a4b
Offliner <42777325+Offliners@users.noreply.github.com>
2020-03-23 13:30:44 +0800

    Create quiz3.c

---
+++ Week4/quiz3.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+#include <wait.h>
+
+int main(){
+
+        int i;
+
+        for(i=0; i<4; i++){
+
+           fork();
+
+        }
+
+        return 0;
+
+}
