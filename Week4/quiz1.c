aa416167903995a2db5132508bae5707677c1b04
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-05 14:27:27 +0800

    Update quiz1.c

--- Week4/quiz1.c
+++ Week4/quiz1.c
@@ -1,33 +1,20 @@
 #include <sys/types.h>
-
 #include <stdio.h>
-
 #include <unistd.h>
-
 #include <wait.h>
 
 int value = 5;
 
 int main(){
-
         pid_t pid;
-
         pid = fork();
-
+        
         if(pid == 0){
-
                 value += 15;
-
         }
-
         else{
-
                 wait(NULL);
-
                 printf("Parent: value: %d\n", value);
-
         }
-
         return 0;
-
 }
