d565430aacd6576508a73c15d4a367015b80009f
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-05 14:28:40 +0800

    Update quiz4.c

--- Week4/quiz4.c
+++ Week4/quiz4.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 #include <sys/types.h>
-
 #include <unistd.h>
 
 #define SIZE 5
@@ -9,39 +7,24 @@
 int nums[SIZE] = {0,1,2,3,4};
 
 int main()
-
 {
-
   int i;
-
   pid_t pid;
-
   pid = fork();
-
- 
 
   if (pid == 0) {
 
-  for (i = 0; i < SIZE; i++) {
-
-  nums[i] *= -i;
-
-  printf("CHILD %d\n",nums[i]); /* LINE X */
-
-  }
-
+        for (i = 0; i < SIZE; i++) {
+                nums[i] *= -i;
+                printf("CHILD %d\n",nums[i]); /* LINE X */
+        }
   }
 
   else if (pid > 0) {
-
-  wait(NULL);
-
-  for (i = 0; i < SIZE; i++)
-
-  printf("PARENT: %d\n",nums[i]); /* LINE Y */
-
+        wait(NULL);
+        for (i = 0; i < SIZE; i++)
+                printf("PARENT: %d\n",nums[i]); /* LINE Y */
   }
 
   return 0;
-
 }
