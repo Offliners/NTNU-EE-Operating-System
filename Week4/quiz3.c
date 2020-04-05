454d0f24680998522e9ed7b24bf6e96093ede5e9
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-05 14:28:16 +0800

    Update quiz3.c

--- Week4/quiz3.c
+++ Week4/quiz3.c
@@ -1,21 +1,14 @@
 #include <sys/types.h>
-
 #include <stdio.h>
-
 #include <unistd.h>
-
 #include <wait.h>
 
 int main(){
-
         int i;
 
         for(i=0; i<4; i++){
-
            fork();
-
         }
 
         return 0;
-
 }
