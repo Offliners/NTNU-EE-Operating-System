5332d80568f706453e705cb318a29814062ebebe
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-15 09:54:28 +0800

    Create openMP3.c

---
+++ Week9/openMP3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <omp.h>
+#define N 4
+
+int main(){
+	int i, j;
+
+	#pragma omp parallel for
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			printf("i:%d, j:%d\n", i, j);
+		}
+	}
+
+
+	return 0;
+}
