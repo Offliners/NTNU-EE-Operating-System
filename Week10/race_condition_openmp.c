4e08e40479b9e5fbc04707a4f394cf93d6b3d8d5
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-15 10:24:09 +0800

    Create race_condition_openmp.c

---
+++ Week10/race_condition_openmp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#define N 10000
+int main(){
+    int n[N];
+    int i;
+	int total = 0;
+	
+	for(i=0; i < N; i++){
+		n[i] = i+1;  
+	}
+	
+	#pragma omp parallel for
+	for(i=0; i < N; i++){
+		total += n[i];
+	}
+	
+	printf("The total is %d\n", total);
+
+	return 0;
+}
